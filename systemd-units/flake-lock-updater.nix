edadfea17ed5d8987e217628e8c2a6c8584335b0
Alessandro Suha <github@plarp.net>
2025-11-27 19:29:36 +0100

    fix: git error during file lock update

--- systemd-units/flake-lock-updater.nix
+++ systemd-units/flake-lock-updater.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 
+let
+  gitUser = "Flake Updater";
+  gitEmail = "flake-updater@localhost";
+in
 {
   systemd.user.services.flake-lock-updater = {
     description = "Update NixOS flake.lock";
@@ -7,8 +11,18 @@
     serviceConfig = {
       Type = "oneshot";
       WorkingDirectory = "/home/plarpoon/nixos-config";
-      ExecStart = "${pkgs.nix}/bin/nix flake update";
     };
+
+    environment = {
+      GIT_AUTHOR_NAME = gitUser;
+      GIT_AUTHOR_EMAIL = gitEmail;
+      GIT_COMMITTER_NAME = gitUser;
+      GIT_COMMITTER_EMAIL = gitEmail;
+    };
+
+    script = ''
+      ${pkgs.nix}/bin/nix flake update --commit-lock-file
+    '';
   };
 
   systemd.user.timers.flake-lock-updater = {
