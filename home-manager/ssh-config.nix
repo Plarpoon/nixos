8d350ffb966dad83fb42018ab79191178ab6781b
Alessandro Suha <github@plarp.net>
2025-11-29 18:33:26 +0100

    fix: the previous fix did not in fact fix anything, changing approach

--- home-manager/ssh-config.nix
+++ home-manager/ssh-config.nix
@@ -1,40 +1,24 @@
-{
-  config,
-  lib,
-  ...
-}:
+{ lib, ... }:
 
 {
-  programs.ssh = {
-    enable = true;
+  home.activation.createSshConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        mkdir -p $HOME/.ssh
+        
+        cat > $HOME/.ssh/config << 'EOF'
+    Host lnu_gitlab
+      HostName gitlab.lnu.se
+      User git
+      IdentityFile ~/.ssh/lnu_gitlab
+      IdentitiesOnly yes
 
-    matchBlocks = {
-      "lnu_gitlab" = {
-        hostname = "gitlab.lnu.se";
-        user = "git";
-        identityFile = "~/.ssh/lnu_gitlab";
-        identitiesOnly = true;
-      };
-
-      "*" = {
-        user = "plarpoon";
-        port = 22;
-        forwardAgent = false;
-        forwardX11 = false;
-        compression = true;
-      };
-    };
-  };
-
-  # Copy SSH config instead of symlinking
-  # So the permissions and ownership will not be wrong
-  home.activation.copySshConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if [ -L "$HOME/.ssh/config" ]; then
-      rm "$HOME/.ssh/config"
-    fi
-
-    # Copy from the generated config
-    cp ${config.home.file.".ssh/config".source} $HOME/.ssh/config
-    chmod 600 $HOME/.ssh/config
+    Host *
+      User plarpoon
+      Port 22
+      ForwardAgent no
+      ForwardX11 no
+      Compression yes
+    EOF
+        
+        chmod 600 $HOME/.ssh/config
   '';
 }
